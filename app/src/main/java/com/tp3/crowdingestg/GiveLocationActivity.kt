b788ed64e28df6ea16111d212b6cc78494f63b9e
Andrefranco98 <andrefranco2016@gmail.com>
2021-05-04 12:03:07 +0100

    iniciospinner

--- app/src/main/java/com/tp3/crowdingestg/GiveLocationActivity.kt
+++ app/src/main/java/com/tp3/crowdingestg/GiveLocationActivity.kt
@@ -12,9 +12,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -50,6 +48,20 @@ class GiveLocationActivity : AppCompatActivity(), View.OnClickListener {
 
         editTextName = findViewById(R.id.textroom)
         ButtonClickMe = findViewById(R.id.button)
+
+
+        val spinner: Spinner = findViewById(R.id.spinnersala)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.salas_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
 
         ButtonClickMe.setOnClickListener(this)
         initBluetooth()
