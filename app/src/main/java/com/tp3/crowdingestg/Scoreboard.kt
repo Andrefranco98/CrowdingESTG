a59d320feabc9888fa6c814273b1c6c93711647e
Marco-Macedo <marcomacedo@ipvc.pt>
2021-05-17 12:18:37 +0100

    + upates

--- app/src/main/java/com/tp3/crowdingestg/Scoreboard.kt
+++ app/src/main/java/com/tp3/crowdingestg/Scoreboard.kt
@@ -9,13 +9,6 @@ import android.os.Bundle
 import android.widget.*
 
 class Scoreboard : AppCompatActivity() {
-    private lateinit var listPairedDevices: ListView
-    private lateinit var listAvailableDevices: ListView
-    private lateinit var adapterPairedDevices: ArrayAdapter<String>
-    private lateinit var adapterAvailableDevices: ArrayAdapter<String>
-    private lateinit var progressScanDevice: ProgressBar
-
-    private var bluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/tp3/crowdingestg/api/EndPoints.kt
+++ app/src/main/java/com/tp3/crowdingestg/api/EndPoints.kt
@@ -4,9 +4,13 @@ package com.tp3.crowdingestg.api
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface EndPoints {
+    @GET("/myslim/api/scoreboard")
+    fun getscoreaboard(): Call<List<scoreboard>>
+
     @FormUrlEncoded
     @POST("/myslim/api/user")
     fun postLogin(
