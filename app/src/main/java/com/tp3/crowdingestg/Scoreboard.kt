d33c19336e057b1e9e9a436291f18690675a71dc
Marco-Macedo <marcomacedo@ipvc.pt>
2021-05-17 11:16:07 +0100

    updates

--- app/src/main/java/com/tp3/crowdingestg/Scoreboard.kt
+++ app/src/main/java/com/tp3/crowdingestg/Scoreboard.kt
@@ -1,11 +1,25 @@
 package com.tp3.crowdingestg
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class Scoreboard : AppCompatActivity() {
+    private lateinit var listPairedDevices: ListView
+    private lateinit var listAvailableDevices: ListView
+    private lateinit var adapterPairedDevices: ArrayAdapter<String>
+    private lateinit var adapterAvailableDevices: ArrayAdapter<String>
+    private lateinit var progressScanDevice: ProgressBar
+
+    private var bluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scoreboard)
     }
-}
+    }
